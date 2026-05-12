0c70ea952c7e7270a0ab268eb61b61023f5ebabd
Rexwt <134538785+Rexwt@users.noreply.github.com>
2026-05-12 16:33:19 -0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+﻿pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
+
+rootProject.name = "NDIReceiver"
+include(":app")
